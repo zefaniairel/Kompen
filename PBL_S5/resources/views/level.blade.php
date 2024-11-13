5a022f2016dc9d53c66198dddac495f9ea770eb6
zefania yizreel nefrit sindhoe <zefaniairel@gmail.com>
2024-11-13 13:42:00 +0700

    Test

--- PBL_S5/resources/views/level.blade.php
+++ PBL_S5/resources/views/level.blade.php
@@ -10,7 +10,7 @@
     <table border="1" cellpadding='2' cellspacing='0'>
         <tr>
             <th>ID</th>
-            <th>Kode Level</th>
+            <th>Kode Level:</th>
             <th>Nama Level</th>
         </tr>
         @foreach($data as $d)
